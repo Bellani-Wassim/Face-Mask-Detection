64921c9a79ace1815ba484fa7399ffb228ff4aa9
Bellani-Wassim <mohamedzargouha@yahoo.com>
2025-05-18 23:44:04 +0100

    implement face detector

--- mask_detector.py
+++ mask_detector.py
@@ -3,55 +3,44 @@ import numpy as np
 from tensorflow.keras.models import load_model
 
 model = load_model('mask_model.h5')
-
 img_size = 224
 
-# Start webcam
+#Load Haar Cascade Face Detector
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+#Start caputre
 cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
+    # convert the frame to gray because haar cascade expect a gray frame
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
-    # Get frame dimensions
-    h, w = frame.shape[:2]
+    for (x, y, w, h) in faces:
+        # crop the face
+        face = frame[y:y+h, x:x+w]
+        # resize
+        face_input = cv2.resize(face, (img_size, img_size))
+        # normalise 
+        face_input = face_input.astype('float32') / 255.0
+        # adds a batch dimension
+        face_input = np.expand_dims(face_input, axis=0)
 
-    # Define a 224x224 square in the center
-    x1 = w // 2 - 112
-    y1 = h // 2 - 112
-    x2 = x1 + 224
-    y2 = y1 + 224
+        pred = model.predict(face_input)[0][0]
+        label = "With mask" if pred < 0.5 else "Without mask"
+        color = (0, 255, 0) if pred < 0.5 else (0, 0, 255)
 
-    # Extract the face region (ROI)
-    face = frame[y1:y2, x1:x2]
+        cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+        cv2.putText(frame, label, (x, y - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
 
-    # Preprocess the face
-    face_input = cv2.resize(face, (img_size, img_size))
-    face_input = face_input.astype('float32') / 255.0
-    face_input = np.expand_dims(face_input, axis=0)
-
-    # Predict using the model
-    pred = model.predict(face_input)[0][0]
-    label = "With Mask" if pred < 0.5 else "Without Mask"
-    color = (0, 255, 0) if pred < 0.5 else (0, 0, 255)
-
-    # Draw guide rectangle (yellow) with instruction
-    cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 255, 0), 2)
-    cv2.putText(frame, "Put your face here", (x1, y1 - 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 0), 2)
-
-    # Show prediction result below the box
-    cv2.putText(frame, label, (x1, y2 + 30),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
-
-    # Show the webcam window
     cv2.imshow("Mask Detection", frame)
 
-    # Exit on pressing 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the camera and close window
 cap.release()
 cv2.destroyAllWindows()
